dcf0067fad3a8603a59aa3f8f1bae880c9968c71
lspn360 <lspn360ip@gmail.com>
2020-04-23 18:43:47 +0900

    ブランチ変更

--- trainings/training3/enquete/form.php
+++ trainings/training3/enquete/form.php
@@ -2,20 +2,25 @@
 <html>
     <head>
         <meta charset="utf-8">
-        <title>�m�F</title>
+        <title>確認</title>
         <link rel="stylesheet" type="text/css" href="../enquete/php.css">
     </head>
     <body>
         <main>
             <section id="main_confirm">
-                <h1>���͓��e</h1>
+                <h1>入力内容</h1>
                 <div class="main_confirm_result">
                     <?php
                     $receiveData = $_POST['data'];
-                    if($receiveData['Name'] == ''){
-                            $receiveData['Name'] = 'Empty';
+                    if($receiveData['name'] === ''){
+                            $receiveData['name'] = '名前が入力されていません';
                         }
-                    foreach ($receiveData as $key => $value){
+                    $keyChange = array(
+                    '名前' => $receiveData['name'],
+                    '電話番号' => $receiveData['tel'],
+                    'メール' => $receiveData['mail']
+                    );
+                    foreach ($keyChange as $key => $value){
                         echo '<p>'.$key.':</p><p>'.$value.'</p>';
                     }
                     ?>
